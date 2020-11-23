c2d3392ce5a192cb321c4edc60233d8c8601f61c
jtoulier <jtoulier@gmail.com>
2020-11-23 18:51:57 -0500

    Categoriesç

---
+++ src/main/java/com/bootcamp/adventureworks/categories/domain/services/impl/CategoriesServiceImpl.java
@@ -0,0 +1,26 @@
+package com.bootcamp.adventureworks.categories.domain.services.impl;
+
+import com.bootcamp.adventureworks.categories.domain.dto.CategoryDto;
+import com.bootcamp.adventureworks.categories.domain.services.CategoriesService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoriesServiceImpl implements CategoriesService {
+    @Override
+    public CategoryDto findById(Integer id) {
+        return new CategoryDto(1, "Montañera");
+    }
+
+    @Override
+    public List<CategoryDto> findAll() {
+        List<CategoryDto> categories = new ArrayList<>();
+
+        categories.add(new CategoryDto(1, "Montañera"));
+        categories.add(new CategoryDto(2, "BMX"));
+
+        return categories;
+    }
+}
